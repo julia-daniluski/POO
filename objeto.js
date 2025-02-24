22ad7e9f16ed1ca6489530717ffc7496dfd72db2
julia-daniluski <julia.o.daniluski@gmail.com>
2025-02-24 15:37:38 -0300

    Update objeto.js

--- objeto.js
+++ objeto.js
@@ -1,10 +1,11 @@
-// criando um objeto de aluno
+
+// Criando um objeto de aluno
 const aluno = {
     nome: "Daniluski",
     idade: 17,
 
     estudar: () => console.log
-    (`${aluno.nome} está estudando.`),
+    (`${aluno.nome} está estudando.`)
 };
 
 console.log(aluno.nome);
