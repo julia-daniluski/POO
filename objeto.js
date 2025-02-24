b5ef8d37ed4ab81259d148de7590c2b802948323
julia-daniluski <julia.o.daniluski@gmail.com>
2025-02-24 12:13:42 -0300

    1

---
+++ objeto.js
@@ -0,0 +1,11 @@
+// criando um objeto de aluno
+const aluno = {
+    nome: "Daniluski",
+    idade: 17,
+
+    estudar: () => console.log
+    (`${aluno.nome} está estudando.`),
+};
+
+console.log(aluno.nome);
+aluno.estudar();
