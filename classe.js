de798644323d861a012d55c12f8c044a06fe1853
julia-daniluski <julia.o.daniluski@gmail.com>
2025-02-25 09:38:59 -0300

    Update classe.js

--- classe.js
+++ classe.js
@@ -1,14 +1,20 @@
 // criando a classe aluno
-class Aluno {
+class Aluno { //modelo para criar alunos/pessoas
     constructor(nome, idade) {
-        // o construtor é um método tradicional
-        this.nome = nome;
+        // o construtor é um método tradicional para criar propriedades
+        this.nome = nome; // significam "este objeto"
         this.idade = idade;
     }
-    estudar = () => console.log (`${this.nome} está estudando`)
+    estudar = () => console.log (`${this.nome} está estudando.`)
+    anos = () => console.log (`Ela tem ${this.idade} anos. `)
+    feliz = () => console.log (`A ${this.nome} está feliz.`)
 }
+
+//podemos criar vários alunos sem repetir o código
 
 const Ray = new Aluno("Ray" , 17)
 
-console.log(Ray.nome)
+console.log(Ray.nome.idade)
 Ray.estudar();
+Ray.anos();
+Ray.feliz();
