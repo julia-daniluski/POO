ba041904d08841ae4ad9436558011f3b6349cc33
julia-daniluski <julia.o.daniluski@gmail.com>
2025-03-24 15:45:43 -0300

    Update abstraction.php

--- abstraction.php
+++ abstraction.php
@@ -1,50 +1,22 @@
 <?php
 
-// evita repetiçãode classes
-
-class pessoa { //modelo para criar pessoa
-    constructor(nome, idade) {
-        if(this.constructor === pessoa) {
-            throw new Error("Imploro para você: não crie isso de novo pelo amor de Deus.")    }
-        this.nome = nome; // significam "este objeto"
-        this.idade = idade;
-}
-    apresentar = () => 
-        console.log (`Olá, me chamo ${this.nome} e tenho ${this.idade} anos de idade.`)
+abstract class Pessoa{
+    public $nome;
+    public function __construct($nome) {
+        $this->nome = $nome;
+    }
 }
 
-// definir extensão
-class aluno extends pessoa{
-    estudar = () => 
-        console.log(`Meu nome é ${this.nome} e eu sou um(a) aluno(a) do SENAI.`);
+class Aluno extends Pessoa{
 }
 
-class professor extends pessoa{
-    ensinar = () => 
-        console.log(`Olá, me chamo ${this.nome}, sou professor(a) no SENAI.`);
+class Professor extends Pessoa{
+    
 }
 
-
-/*
-try {
-    const p = new pessoa("Carlos", 40); // Isso vai gerar um erro
-} catch (e) {
-    console.log(e.message); // Isso vai capturar e exibir a mensagem de erro
-}
-    */
-
-// criar objeto
-const Lais = new aluno("Lais" , 16)
-const Alexya = new professor ("Alexya" , 34)
-
-//chamar o objeto
-Lais.apresentar();
-Alexya.apresentar();
-Lais.estudar();
-Alexya.ensinar();
-
+$alexya = new Aluno ("Alexya");
+echo $alexya->nome . " é ela!";
 ?>
-
 
 
 
