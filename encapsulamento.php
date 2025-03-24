5065acfdde72dd574d3628fbfbcda2104871dc86
julia-daniluski <julia.o.daniluski@gmail.com>
2025-03-24 14:18:09 -0300

    Create encapsulamento.php

---
+++ encapsulamento.php
@@ -0,0 +1,24 @@
+<?php
+
+// Criando a classe Aluno
+class Aluno {
+    // Propriedades
+    public $nome;
+    public $idade;
+    private $nota;
+    
+    // Construtor
+    public function __construct($nome, $idade, $nota) {
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->nota = $nota;
+    }
+    public function verNota(){
+        return $this->nota;
+    }
+}
+
+$aluno1 = new Aluno ("Dani", 1209, 0.5 );
+echo $aluno1->nome;
+echo $aluno1->verNota();
+?>
